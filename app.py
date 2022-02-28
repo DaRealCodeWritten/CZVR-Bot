974a29d6fc54f6427881b193b28f3f1c06118808
DaRealCodeWritten <sswiftgamer440@gmail.com>
2022-02-28 01:22:16 -0800

    Amend redirect URLs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from urllib import parse
 from flask import Flask, render_template, request, redirect
 
 
@@ -21,13 +22,14 @@ def authorized_discord():
         "client_id": os.environ.get("CLIENT_ID"),
         "client_secret": os.environ.get("CLIENT_SECRET"),
         "grant_type": "authorization_code",
-        "redirect_uri": "https://czvr-bot.herokuapp.com/discord/success"
+        "redirect_uri": parse.urlparse("https://czvr-bot.herokuapp.com/discord/success/")
     }
     header = {
         "Content-Type": "application/x-www-form-urlencoded"
     }
     r = requests.post("https://discord.com/api/v8/oauth2/token", headers=header, data=data)
     print(r.json())
+    return redirect("https://czvr-bot.herokuapp.com/discord/success")
 
 
 @app.route("/discord/success")
