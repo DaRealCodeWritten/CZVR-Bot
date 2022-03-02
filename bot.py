2dbb8c37328be332b5fb903e4128902ad1ae55bb
DaRealCodeWritten <sswiftgamer440@gmail.com>
2022-03-01 20:32:13 -0800

    make bot run, c/dos, c/dos/run, run dos run

--- bot.py
+++ bot.py
@@ -14,6 +14,14 @@ db = psycopg2.connect(
 dbcrs = db.cursor()
 
 
+def is_dev():
+    def predicate(ctx):
+        return ctx.author.id == 589477926961938443
+
+    return discord.ext.commands.check(predicate)
+
+
+@is_dev()
 @bot.command()
 async def fupdate(ctx):
     """Force a complete recall of the database"""
@@ -25,3 +33,18 @@ async def fupdate(ctx):
         rows = ret.fetchall()
         for user in rows:
             print(user)
+
+
+@is_dev()
+@bot.command()
+async def dbexec(ctx, *, query):
+    try:
+        out = dbcrs.execute(query)
+        await ctx.author.send(out)
+        await ctx.send("Command completed")
+    except Exception as e:
+        print(e)
+        await ctx.send("Command failed for reason: {}".format(e))
+
+
+bot.run(os.environ.get("BOT_TOKEN"))
