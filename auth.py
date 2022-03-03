406f62102cbdb62744423f5ad5645836994a381d
DaRealCodeWritten <sswiftgamer440@gmail.com>
2022-03-03 00:44:30 -0800

    Fix newline issue

--- auth.py
+++ auth.py
@@ -2,6 +2,7 @@ def return_auth():
     foo = {}
     with open("auth.config") as config:
         for line in config:
+            line = line.strip("\n")
             data = line.split(":")
             foo[data[0]] = data[1]
     return foo
