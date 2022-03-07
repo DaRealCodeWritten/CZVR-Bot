53d5ab27ad6481b7ca9d9718d8a3c71a2989520c
DaRealCodeWritten <sswiftgamer440@gmail.com>
2022-03-07 00:47:40 -0800

    Update some stuff

--- app.py
+++ app.py
@@ -51,32 +51,41 @@ def discord_success():
 @app.route("/vatsim/oauth")
 def vatsim_link():
     """Redirect URL to link to VATSIM"""
-    data = {
-        "client_id": config["VATSIM_CLIENT_ID"],
-        "client_secret": config["VATSIM_CLIENT_SECRET"],
-        "code": request.args.get("code"),
-        "grant_type": "authorization_code"
-    }
-    returned = requests.post(f"{config['VATSIM_AUTH']}/oauth/token", data=data)
-    jsonify = returned.json()
-    token = jsonify["access_token"]
-    headers = {
-        "Authorization": token,
-        "Accept": "application/json"
-    }
-    user_data = requests.post(f"{config['VATSIM_AUTH']}/api/user", headers=headers)
-    juser = user_data.json()
-    cid = int(juser["data"]["cid"])
-    crs = db.cursor()
     try:
-        crs.execute(f"INSERT INTO {config['DATABASE_TABLE']} VALUES (? ? ?)", (cid, 0, 0))
-    except:
-        pass
-    crs.close()
-    db.commit()
+        data = {
+            "client_id": config["VATSIM_CLIENT_ID"],
+            "client_secret": config["VATSIM_CLIENT_SECRET"],
+            "code": request.args.get("code"),
+            "grant_type": "authorization_code",
+            "redirect_uri": "http://server.czvr-bot.com:500/vatsim/oauth"
+        }
+        returned = requests.post(f"{config['VATSIM_AUTH']}/oauth/token", data=data)
+        jsonify = returned.json()
+        token = jsonify.get("access_token")
+        if token is None: # Site probably threw an error, FAIL
+            return "Failed"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/json"
+        }
+        user_data = requests.post(f"{config['VATSIM_AUTH']}/api/user", headers=headers)
+        juser = user_data.json()
+        udata = juser.get("data")
+        if udata is None: # Site threw an error AGAIN FAIL
+            return "Failed"
+        cid = int(udata.get("cid"))
+        crs = db.cursor()
+        try:
+            crs.execute(f"INSERT INTO {config['DATABASE_TABLE']} VALUES (? ? ?)", (cid, 0, 0))
+        except:
+            pass
+        crs.close()
+        db.commit()
+    except Exception as e:
+        print(e)
 
 
 try:
-    app.run(None, 80)
+    app.run("0.0.0.0", 80)
 except Exception as e:
     print(e)
